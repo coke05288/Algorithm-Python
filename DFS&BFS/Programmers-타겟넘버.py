0c73ee75889133c883de23e99047afb7bc9039f3
YoungJun <coke05288@gmail.com>
2024-07-29 23:53:47 +0900

    checked Programmers 타겟넘버 : 240729

--- DFS&BFS/Programmers-타겟넘버.py
+++ DFS&BFS/Programmers-타겟넘버.py
@@ -24,5 +24,14 @@ def solution(numbers, target):
     
     return cnt
 
+def recur_sol(numbers, target):
+    if not numbers and target == 0:
+        return 1
+    elif not numbers : 
+        return 0
+    return recur_sol(numbers[1:], target - numbers[0]) + recur_sol(numbers[1:], target + numbers[0])
+
+
 print(solution([1, 1, 1, 1, 1], 3))
-print(solution([4, 1, 2, 1], 4))
+print(recur_sol([4, 1, 2, 1], 4))
+
