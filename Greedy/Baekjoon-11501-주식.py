56cd702ac5c2dc71667fdee736d5d6557989c334
coke05288 <coke05288@gmail.com>
2024-08-06 15:44:32 +0900

    fixed Baekjoon-11501-주식 : 20240806

--- Greedy/Baekjoon-11501-주식.py
+++ Greedy/Baekjoon-11501-주식.py
@@ -2,25 +2,29 @@
 ## 백준 11501 주식
 ## https://www.acmicpc.net/problem/11501
 
+import sys
+input = sys.stdin.readline
+
 T = int(input())
 
 def solution(day_prices):
     answer = 0
-    max_price = max(day_prices)
+    max_price = 0
 
-    for i in range(len(day_prices)):
-        if max_price == day_prices[i] and i != len(day_prices)-1:
-            max_price = max(day_prices[i+1:])
-            continue
+    for i in range(len(day_prices) - 1, -1, -1):
+        if max_price < day_prices[i]:
+            max_price = day_prices[i]
         else:
             answer += max_price - day_prices[i]
-
     return answer
     
 
 for _ in range(T):
     N = int(input())
     day_prices = list(map(int, input().split()))
-    
-    print(solution(day_prices))
+
+    if day_prices[0] * 3 == sum(day_prices):
+        print(0)
+    else:
+        print(solution(day_prices))
 
